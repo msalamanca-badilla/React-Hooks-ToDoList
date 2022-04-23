00b90d7df2c24152666664e2a149aa3b25575b27
Marianne <m.salamanca21@live.ca>
2022-04-23 13:29:46 -0400

    remove toDoData from toDoinput component

--- src/Components/ToDoInput/ToDoInput.jsx
+++ src/Components/ToDoInput/ToDoInput.jsx
@@ -2,27 +2,19 @@ import React, { useState } from "react";
 
 export default function TodoList(){
     const [toDoInput, setToDoInput]= useState('')
-    const [toDoData, setToDoData] = useState('No ToDos Added')
 
     const handleChange = evt => {
         setToDoInput(evt.target.value)
     }
     const addToDo = evt => {
         evt.preventDefault()
-        setToDoData({
-            toDoData: 'added',
-        })
         setToDoInput({
             toDoInput:' '
         })
-        console.log('input',  toDoInput)
-        console.log('ida',  toDoData)
     }
 
     return(
         <div className = 'ToDoList'>
-                <div>{toDoData.toString()}</div>
-                <br />
             <form onSubmit={(evt)=>addToDo(evt)}>
                 <input 
                     type="text" 
