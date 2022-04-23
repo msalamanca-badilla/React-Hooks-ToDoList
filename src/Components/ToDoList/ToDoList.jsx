b4d93c978558eb5ae88b6bc5fc65b6ca9b386202
Marianne <m.salamanca21@live.ca>
2022-04-23 12:48:58 -0400

    add addToDo for submit

--- src/Components/ToDoList/ToDoList.jsx
+++ src/Components/ToDoList/ToDoList.jsx
@@ -4,22 +4,32 @@ export default function TodoList(){
     const [toDoInput, setToDoInput]= useState('')
     const [toDoData, setToDoData] = useState('No ToDos Added')
 
-    const handleChange=evt =>{
+    const handleChange= evt => {
         setToDoInput(evt.target.value)
     }
-
+    const addToDo= evt => {
+        evt.preventDefault()
+        const toDo = [toDoInput]
+        setToDoData({
+            toDo,
+            toDoInput: ' '
+        })
+    }
 
     return(
         <div className = 'ToDoList'>
-            <div>{toDoData}</div>
-            <br />
-            <input 
-            type="text" 
-            value={toDoInput}
-            onChange={handleChange}
-            />
-            <button >Add To Do</button>
-            <button>Remove To Do</button>
+            
+                <div>{toDoInput}</div>
+                <br />
+            <form onSubmit={addToDo}>
+                <input 
+                type="text" 
+                value={toDoInput}
+                onChange={handleChange}
+                />
+                <button onSubmit={addToDo}>Add To Do</button>
+                {/* <button>Remove To Do</button> */}
+            </form>
         </div>
     )
 }
