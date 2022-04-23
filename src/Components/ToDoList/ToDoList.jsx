7f2afa1ac825faab40b4ce38c6e9c736a71bb7d6
Marianne <m.salamanca21@live.ca>
2022-04-23 11:51:54 -0400

    add handleChange for toDoInput

--- src/Components/ToDoList/ToDoList.jsx
+++ src/Components/ToDoList/ToDoList.jsx
@@ -1,21 +1,24 @@
 import React, { useState } from "react";
 
 export default function TodoList(){
-    const [toDoData, setToDoData]= useState({
-        toDo: 'No To Dos Added',
-        toDoInput:''
-    })
+    const [toDoInput, setToDoInput]= useState('')
+    const [toDoData, setToDoData] = useState('No ToDos Added')
+
+    const handleChange=evt =>{
+        setToDoInput(evt.target.value)
+    }
 
 
     return(
         <div className = 'ToDoList'>
-            <div>{toDoData.toDo}</div>
+            <div>{toDoData}</div>
             <br />
             <input 
             type="text" 
-            value={toDoData.toDoInput}
+            value={toDoInput}
+            onChange={handleChange}
             />
-            <button>Add To Do</button>
+            <button >Add To Do</button>
             <button>Remove To Do</button>
         </div>
     )
